631c91cfb70c9dfe58434aa2e604c7f7a0500977
Sobczak Malgorzata <sobczakmalgorz@gmail.com>
2016-06-09 17:48:02 +0200

    Changing hill climbing to find minimum and changing retuned value

--- hill_climbing.R
+++ hill_climbing.R
@@ -1,6 +1,6 @@
 hill_climbing <- function(n, starting_piont, function_number, initial_step_size, epsilon = 0.0001, max_iter = 100, initial_acceleration=1.2) { #) {
   current_point <- starting_piont
-  step_size <- initial_step_size
+  step_size <- rep(initial_step_size,n)
   acceleration <- initial_acceleration
   candidate <- list()
   candidate[1] <- -acceleration
@@ -12,13 +12,13 @@ hill_climbing <- function(n, starting_piont, function_number, initial_step_size,
   while(index < max_iter){
     before <- cec2013(function_number, current_point)
     for (i in (1:(length(current_point)))){
-      best <- -1
-      best_score <- -Inf
+      best <- 1
+      best_score <- Inf
       for(j in (1:5)) {
         current_point[i] = current_point[i] + step_size[i] * candidate[[j]]
         temp = cec2013(function_number,current_point)
         current_point[i] = current_point[i] - step_size[i] * candidate[[j]]
-        if(temp > best_score) {
+        if(temp < best_score) {
           best_score = temp
           best = j
         }
@@ -28,22 +28,23 @@ hill_climbing <- function(n, starting_piont, function_number, initial_step_size,
         step_size[i] = step_size[i] * candidate[[best]]
       }
     }
-    condition <- (cec2013(function_number, current_point) - before)
-    if ((condition < epsilon) | ((cec2013(function_number, current_point)==Inf) & (before == Inf))){
-      return(current_point)
+    current_cec <- cec2013(function_number, current_point)
+    condition <- abs(current_cec - before)
+    if ((condition < epsilon) | ((current_cec==Inf) & (before == Inf))){
+      return(c(current_point, current_cec))
     }
     index <- index + 1
   }
   
-  return(current_point)
+  return(c(current_point, current_cec))
 }
 
 
 multi_start_hill_climbing <- function(n, data, function_number, initial_step_size, epsilon = 0.0001, max_iter = 100, initial_acceleration = 1.2){
   
   a <- list()
-  for(i in (1:(length(data)))) {
-    a[[i]] <- hill_climbing(n, data[i,], function_number, initial_step_size, epsilon, max_iter, initial_acceleration)
+  for(i in (1:(length(data[,1])))) {
+    a[[i]] <- c(data[i,],hill_climbing(n, data[i,], function_number, initial_step_size, epsilon, max_iter, initial_acceleration))
   }
   return (a)
 }
